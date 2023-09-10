c6cf1529054dd43644d615f3c64703c2a5c0dd6d
Kafonek <matt.kafonek@noteable.io>
2023-09-10 12:38:03 -0400

    add WsSession concept

--- demo-server/src/ws_rpc/mod.rs
+++ demo-server/src/ws_rpc/mod.rs
@@ -1,8 +1,11 @@
-use futures_util::{SinkExt, StreamExt};
+use std::{collections::HashMap, sync::Arc};
+
+use futures_util::{stream::SplitSink, SinkExt, StreamExt};
 use poem::{
     web::websocket::{Message, WebSocket, WebSocketStream},
     IntoResponse,
 };
+use tokio::sync::Mutex;
 
 #[poem::handler]
 pub async fn ws_upgrade(ws: WebSocket) -> impl IntoResponse {
@@ -11,13 +14,10 @@ pub async fn ws_upgrade(ws: WebSocket) -> impl IntoResponse {
 
 async fn ws_handle(socket: WebSocketStream) {
     let (mut tx, mut rx) = socket.split();
+    let session = WsSession::new(tx);
     while let Some(msg) = rx.next().await {
         match msg {
-            Ok(Message::Text(msg)) => {
-                println!("Received message: {}", msg);
-                let echo = Message::Text(msg);
-                tx.send(echo).await.unwrap();
-            }
+            Ok(Message::Text(msg)) => session.handle_message(msg).await,
             Err(e) => {
                 println!("Error: {}", e);
             }
@@ -25,5 +25,23 @@ async fn ws_handle(socket: WebSocketStream) {
                 println!("Unknown message {:?}", msg);
             }
         }
+    }
+}
+
+#[derive(Clone)]
+pub struct WsSession {
+    tx: Arc<Mutex<SplitSink<WebSocketStream, Message>>>,
+}
+
+impl WsSession {
+    pub fn new(ws_tx: SplitSink<WebSocketStream, Message>) -> Self {
+        let tx = Arc::new(Mutex::new(ws_tx));
+        Self { tx }
+    }
+
+    pub async fn handle_message(&self, msg: String) {
+        println!("Received message: {}", msg);
+        let echo = Message::Text(msg);
+        self.tx.lock().await.send(echo).await.unwrap();
     }
 }
