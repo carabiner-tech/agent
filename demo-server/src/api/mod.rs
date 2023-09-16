87b0e816feae2d3a8aadb0c477b989f353c53a5f
Kafonek <matt.kafonek@noteable.io>
2023-09-16 16:05:51 -0400

    error helper

--- demo-server/src/api/mod.rs
+++ demo-server/src/api/mod.rs
@@ -15,10 +15,7 @@ use poem_openapi::{
     payload::{Json, PlainText},
     OpenApi,
 };
-use rpc::operations::{
-    list_files::ListFilesRequest,
-    time::{SystemTimeRequest, SystemTimeResponse},
-};
+use rpc::operations::{list_files::ListFilesRequest, time::SystemTimeRequest};
 
 use crate::{
     dependencies::{Conversation, ConversationHeader},
@@ -30,6 +27,11 @@ use crate::{
 // in App state rather than as part of the Api struct (which routes could access with
 // &self), because the ws endpoint needs to get access to them too.
 pub struct Api;
+
+fn rpc_error(e: rpc::RpcResponse) -> Error {
+    let rpc_error = e.into_rpc_error().unwrap();
+    Error::from_string(rpc_error.to_string(), poem::http::StatusCode::BAD_REQUEST)
+}
 
 #[OpenApi]
 impl Api {
@@ -45,7 +47,7 @@ impl Api {
         conversation_header: ConversationHeader,
         conversation_session_map: Data<&ConversationSessionMap>,
         ws_session_manager: Data<&WsSessionManager>,
-    ) -> Result<PlainText<String>, Error> {
+    ) -> poem::Result<PlainText<String>> {
         let conversation_id = conversation_header.0;
         let agent_id = uuid::Uuid::parse_str(&agent_id).map_err(|err| {
             let s = format!("Agent ID must be a valid UUID: {}", err);
@@ -64,7 +66,7 @@ impl Api {
 
     /// RPC operation to get the current system time for the active Agent in the conversation
     #[oai(path = "/current_time", method = "post", operation_id = "current_time")]
-    async fn current_time(&self, conversation: Conversation) -> Result<PlainText<String>, Error> {
+    async fn current_time(&self, conversation: Conversation) -> poem::Result<PlainText<String>> {
         let req = SystemTimeRequest {};
         let resp = conversation.session.send_rpc(req.into()).await;
         match resp.into_system_time() {
@@ -72,14 +74,7 @@ impl Api {
                 let s = format!("Current time: {}", resp.time);
                 Ok(PlainText(s))
             }
-            Err(e) => {
-                let rpc_error = e.into_rpc_error().unwrap();
-
-                Err(Error::from_string(
-                    rpc_error.to_string(),
-                    poem::http::StatusCode::BAD_REQUEST,
-                ))
-            }
+            Err(e) => Err(rpc_error(e)),
         }
     }
 
@@ -89,7 +84,7 @@ impl Api {
         &self,
         body: Json<ListFilesRequest>,
         conversation: Conversation,
-    ) -> Result<PlainText<String>, Error> {
+    ) -> poem::Result<PlainText<String>> {
         let req = body.0;
         let resp = conversation.session.send_rpc(req.into()).await;
         match resp.into_list_files() {
@@ -103,14 +98,7 @@ impl Api {
                 }
                 Ok(PlainText(s))
             }
-            Err(e) => {
-                let rpc_error = e.into_rpc_error().unwrap();
-
-                Err(Error::from_string(
-                    rpc_error.to_string(),
-                    poem::http::StatusCode::BAD_REQUEST,
-                ))
-            }
+            Err(e) => Err(rpc_error(e)),
         }
     }
 }
